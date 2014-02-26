8cd3f596aeb46ff84122d779dc6ca76aa676db6e
Jordan Morano <jordan.morano@gmail.com>
2014-02-26 11:48:11 -0500

    Add hide comments and fix urls for viewing correct comment / article id

--- js/haxor_news.js
+++ js/haxor_news.js
@@ -15,7 +15,6 @@ HaxorNews.callArticles = function(){
     dataType: 'JSON',
     data: { backdoor_user_id: 5 },
     success: function(response){
-      console.log(response.articles);
       HaxorNews.renderArticles(response.articles);
     }
   });
@@ -41,9 +40,9 @@ HaxorNews.renderArticle = function(article){
   htmlString = '<h3><a href="' + article.url + '">' + article.title + '</a></h3>';
   htmlString = htmlString + '<p>' + article.score + ' points by ' + article.user_email
     + ' about ' + hoursAgo + ' hours ago | '
-    + '<a href="' + HaxorNews.rootUrl + 'articles/3/vote?direction=up" id="vote_up">Upvote</a> | '
-    + '<a href="' + HaxorNews.rootUrl + 'articles/3/vote?direction=down" id="vote_up">Downvote</a> | '
-    + '<a href="' + HaxorNews.rootUrl + 'articles/3/comments" id="comments">' + article.comments_count + ' comments</a></p>';
+    + '<a href="' + HaxorNews.rootUrl + 'articles/' + article.id + '/vote?direction=up" id="vote_up">Upvote</a> | '
+    + '<a href="' + HaxorNews.rootUrl + 'articles/' + article.id + '/vote?direction=down" id="vote_up">Downvote</a> | '
+    + '<a href="' + HaxorNews.rootUrl + 'articles/'+ article.id +'/comments" id="comments">' + article.comments_count + ' comments</a></p>';
   $article = $('<div class="article">').append(htmlString).append($('<div class="comment_list">'));
 
   $('#article_list').append($article);
@@ -58,7 +57,6 @@ HaxorNews.showComment = function(event){
     dataType: 'JSON',
     data: { backdoor_user_id: 5 },
     success: function(response){
-      console.log(response.comments)
       HaxorNews.renderComments(response.comments, $commentList);
     }
   });
@@ -74,6 +72,7 @@ HaxorNews.renderComments = function(comments, $list) {
         '<textarea id="comment_body" name="comment[body]" placeholder="Enter comment..."></textarea>' +
         '<input name="commit" type="submit" value="Add Comment" />' +
       '</form>';
+    $list.append('<button>Hide</button>').click(HaxorNews.removeComments);
     $list.append(formString);
 
   for (; i < length; ) {
@@ -91,12 +90,17 @@ HaxorNews.renderComment = function(comment) {
   htmlString = '';
   htmlString = htmlString + '<p>' + comment.score + ' points by ' + comment.user_email
     + ' about ' + hoursAgo + ' hours ago | '
-    + '<a href="' + HaxorNews.rootUrl + 'comments/3/vote?direction=up" id="vote_up">Upvote</a> | '
-    + '<a href="' + HaxorNews.rootUrl + 'comments/3/vote?direction=down" id="vote_up">Downvote</a></p>';
+    + '<a href="' + HaxorNews.rootUrl + 'comments/'+ comment.id + '/vote?direction=up" id="vote_up">Upvote</a> | '
+    + '<a href="' + HaxorNews.rootUrl + 'comments/'+ comment.id + '/vote?direction=down" id="vote_up">Downvote</a></p>';
   htmlString = htmlString + '<p>' + comment.body + '</p>';
   $comment = $('<div class="comment">').append($('<hr>')).append(htmlString);
   return $comment;
 };
+
+HaxorNews.removeComments = function(event) {
+  event.preventDefault();
+  $(event.target).parent().empty();
+}
 
 
 
